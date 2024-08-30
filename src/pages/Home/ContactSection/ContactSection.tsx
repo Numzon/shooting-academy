b49cc84b6b0a0463010b0f03a8e63b3911b33d2c
Daniel Młynarczyk <daniel.mlynarczyk@stxnext.pl>
2024-08-30 16:52:40 +0200

    added contact section

---
+++ src/pages/Home/ContactSection/ContactSection.tsx
@@ -0,0 +1,43 @@
+import HeadingPrimary from '../../../components/HeadingPrimary';
+import TextField from '../../../components/TextField';
+import Button from '../../../components/Button';
+import { RadioButton, RadioGroup } from '../../../components/RadioGroup';
+
+export const ContactSection = () => {
+  return (
+    <div className="contact-section">
+      <div className="row">
+        <div className="col-12">
+          <div className="contact">
+            <div className="contact__form">
+              <div className="u-margin-bottom-medium">
+                <HeadingPrimary>Lorem ipsum, dolor sit amet.</HeadingPrimary>
+              </div>
+              <form action="#" className="form">
+                <TextField id="full_name" type="text" placeholder="Full name" required>
+                  Full name
+                </TextField>
+                <TextField id="email_address" type="email" placeholder="Email address" required>
+                  Email address
+                </TextField>
+                <RadioGroup>
+                  <RadioButton id="first" name="test">
+                    Lorem impsum
+                  </RadioButton>
+                  <RadioButton id="second" name="test">
+                    Ipsume lorems ses
+                  </RadioButton>
+                </RadioGroup>
+                <div className="u-margin-top-huge contact__button">
+                  <Button type="tertiary">Send now!</Button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactSection;
