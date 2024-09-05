002caab5965cb64685a546b9bca783dc9644fe4b
Daniel Młynarczyk <daniel.mlynarczyk@stxnext.pl>
2024-09-05 19:46:28 +0200

    created cards for mobile users

--- src/components/BundleCard/BundleCardPrice/BundleCardPrice.tsx
+++ src/components/BundleCard/BundleCardPrice/BundleCardPrice.tsx
@@ -5,7 +5,7 @@ type BundleCardPriceProps = {
 
 export const BundleCardPrice = ({ price, heading }: BundleCardPriceProps) => {
   return (
-    <div className="bundle-card__price-box u-margin-top-medium">
+    <div className="bundle-card__price-box">
       <p className="bundle-card__price-text">{heading}</p>
       <p className="bundle-card__price">{price}</p>
     </div>
